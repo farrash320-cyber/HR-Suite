a48a6dafe081e33e7355485ce1038bc617a3e5c3
Al Farras <farrash320@gmail.com>
2026-06-17 15:35:06 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -330,3 +330,35 @@ window.onload = ()=>{
     );
 
 };
+
+// ====================================
+// PROFILE DROPDOWN
+// ====================================
+
+let profile =
+document.querySelector(
+".profile"
+);
+
+let dropdown =
+document.querySelector(
+".dropdown-menu"
+);
+
+profile.addEventListener(
+"click",()=>{
+
+    if(
+    dropdown.style.display==="block"
+    ){
+
+        dropdown.style.display="none";
+
+    }
+    else{
+
+        dropdown.style.display="block";
+
+    }
+
+});
