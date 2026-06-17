21689fda950458e74a22697ab321524a50d32ec8
Al Farras <farrash320@gmail.com>
2026-06-17 17:09:16 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -351,3 +351,35 @@ console.log(
 "NEXORA Human Resource Suite Ready"
 
 );
+
+// ======================================
+// PROFILE DROPDOWN
+// ======================================
+
+let profile =
+document.querySelector(
+".profile"
+);
+
+let dropdown =
+document.querySelector(
+".dropdown-menu"
+);
+
+profile.addEventListener(
+"click",()=>{
+
+if(
+dropdown.style.display==="block"
+){
+
+dropdown.style.display="none";
+
+}
+else{
+
+dropdown.style.display="block";
+
+}
+
+});
